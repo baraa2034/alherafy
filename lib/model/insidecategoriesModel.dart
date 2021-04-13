0d5e58040d412b8465ad20f7582835afe1b5c085
unknown <baraa.2034@gmail.com>
2021-04-13 10:19:14 +0300

    alherafy

---
+++ lib/model/insidecategoriesModel.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+class insideCategoriesModle{
+  String name;
+  String number;
+  String descrption;
+
+  String imagedctwo;
+  String imagedc;
+  String image;
+  String userId;
+  String insideId;
+
+  insideCategoriesModle({this.name,this.image,this.descrption,this.imagedc,this.number,this.imagedctwo});
+  insideCategoriesModle.fromMap(Map map){
+
+    this.name=map['name'];
+    this.image=map['image'];
+    this.userId=map['userId'];
+    this.insideId=map['insideId'];
+    this.number=map['number'];
+    this.imagedc=map['imagedc'];
+    this.descrption=map['descrption'];
+    this.imagedctwo=map['imagedctwo'];
+
+  }
+  toJson(){
+    return {
+
+      'image':this.image,
+      'name':this.name,
+      'userId':this.userId,
+      'insideId':this.insideId,
+      'number':this.number,
+      'imagedc':this.imagedc,
+      'descrption':this.descrption,
+      'imagedctwo':this.imagedctwo
+
+
+
+
+
+
+    };
+  }
+
+}
